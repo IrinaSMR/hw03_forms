6e6977972105083b7680340e6cc034e7f2cfc466
Ирина Смирнова <ismirnova83@yandex.ru>
2022-05-13 17:48:47 +0300

    Final status

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -22,25 +22,25 @@ urlpatterns = [
     path(
         'password_change/',
         PasswordChangeView.as_view(
-                                   template_name='users/password_change_form.html'),
-                                   name='password_change'
+            template_name='users/password_change_form.html'),
+        name='password_change'
     ),
     path(
         'password_change/done/',
         PasswordChangeDoneView.as_view(
-                                       template_name='users/password_change_done.html'),
-                                       name='password_change_done'
+            template_name='users/password_change_done.html'),
+        name='password_change_done'
     ),
     path(
         'password_reset/',
         PasswordResetView.as_view(
-                                  template_name='users/password_reset_form.html'),
-                                  name='password_reset'
+            template_name='users/password_reset_form.html'),
+        name='password_reset'
     ),
     path(
         'password_reset/done/',
         PasswordResetDoneView.as_view(
-                                      template_name='users/password_reset_done.html'),
-                                      name='password_reset_done'
+            template_name='users/password_reset_done.html'),
+        name='password_reset_done'
     ),
 ]
